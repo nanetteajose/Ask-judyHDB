37c99f5d787378e921eef654e5cc7815ab256bf8
Judy <judybot@outlook.com>
2016-10-04 16:38:01 +0800

    updated config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.host = process.env.HOST || "https://judybot.documents.azure.com:443/";
 config.authKey = process.env.AUTH_KEY || "r0DuZjNGT93s7ZewedJJNYwSYvReFkmXwEctPF57x1RPUdvdtHeLzeyxhKl7gqw3YP5E2pqFBgGXw3ZtAetF3Q==";
-config.databaseId = "ToDoList";
+config.databaseId = "judybotdb";
 config.collectionId = "Items";
 
 module.exports = config;
